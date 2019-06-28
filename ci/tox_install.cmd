3851870a06897ad9d874b323a5f42117fd838553
Charlles Abreu <abreu@eq.ufrj.br>
2019-06-28 20:47:58 +0200

    Changes in tox testing

--- ci/tox_install.cmd
+++ ci/tox_install.cmd
@@ -6,7 +6,10 @@ for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
 pip install %ALL_BUT_FIRST%
 goto :end
 ::batch
+conda create --prefix=$1 --yes python=3.7
 conda install --prefix=$1 --yes -c omnia openmm
+conda install --prefix=$1 --yes pytest pytest-cov sphinx sphinx_rtd_theme
+conda install --prefix=$1 --yes -c conda-forge future_fstrings sphinxcontrib-bibtex
 pip install ${@:2}
 exit $?
 :<<"::done"
